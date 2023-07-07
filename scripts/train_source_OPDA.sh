0e339169838b1b2da42bc96593459b79d558f23f
sanqingqu <sanqingqu@gmail.com>
2023-07-07 16:36:12 +0800

    add DomainNet

--- scripts/train_source_OPDA.sh
+++ scripts/train_source_OPDA.sh
@@ -16,3 +16,8 @@ python train_source.py  --dataset OfficeHome --s_idx 3  --target_label_type OPDA
 
 echo "OPDA SOURCE TRAIN ON VisDA"
 python train_source.py --backbone_arch resnet50 --dataset VisDA --s_idx 0  --target_label_type OPDA --epochs 10 --lr 0.001
+
+echo "OPDA SOURCE TRAIN ON DomainNet"
+python train_source.py  --dataset DomainNet --s_idx 0  --target_label_type OPDA --epochs 50 --lr 0.01 
+python train_source.py  --dataset DomainNet --s_idx 1  --target_label_type OPDA --epochs 50 --lr 0.01 
+python train_source.py  --dataset DomainNet --s_idx 2  --target_label_type OPDA --epochs 50 --lr 0.01 
--- scripts/train_target_OPDA.sh
+++ scripts/train_target_OPDA.sh
@@ -29,3 +29,11 @@ python train_target.py --dataset OfficeHome --s_idx 2 --t_idx 3 --lr 0.001  --la
 python train_target.py --dataset OfficeHome --s_idx 3 --t_idx 0 --lr 0.001  --lam_psd $lam_psd --target_label_type OPDA
 python train_target.py --dataset OfficeHome --s_idx 3 --t_idx 1 --lr 0.001  --lam_psd $lam_psd --target_label_type OPDA
 python train_target.py --dataset OfficeHome --s_idx 3 --t_idx 2 --lr 0.001  --lam_psd $lam_psd --target_label_type OPDA
+
+echo "OPDA Adaptation ON DomainNet"
+python train_target.py --dataset DomainNet --s_idx 0 --t_idx 1 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
+python train_target.py --dataset DomainNet --s_idx 0 --t_idx 2 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
+python train_target.py --dataset DomainNet --s_idx 1 --t_idx 0 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
+python train_target.py --dataset DomainNet --s_idx 1 --t_idx 2 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
+python train_target.py --dataset DomainNet --s_idx 2 --t_idx 0 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
+python train_target.py --dataset DomainNet --s_idx 2 --t_idx 1 --lr 0.0001  --lam_psd $lam_psd --target_label_type OPDA --epochs 10
